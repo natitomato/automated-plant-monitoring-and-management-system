4a3c6ffb5f3c571799cf8586210b40f1a85ff893
thundercookie <57059815+natitomato@users.noreply.github.com>
2021-05-23 01:17:11 +0530

    Update test_code.ino
    removed comments

--- Sensors/SoilMoistureSensor/test_code.ino
+++ Sensors/SoilMoistureSensor/test_code.ino
@@ -1,14 +1,14 @@
-const int AirValue = 645;   //you need to replace this value with Value_1
-const int WaterValue = 330;  //you need to replace this value with Value_2
+const int AirValue = 645;
+const int WaterValue = 330;
 int intervals = (AirValue - WaterValue)/3;
 int soilMoistureValue = 0;
 float value;
 float perc;
 void setup() {
-  Serial.begin(9600); // open serial port, set the baud rate to 9600 bps
+  Serial.begin(9600);
 }
 void loop() {
-soilMoistureValue = analogRead(A0);  //put Sensor insert into soil
+soilMoistureValue = analogRead(A0);
 value = soilMoistureValue-WaterValue;
 //Serial.println(value);
 perc = 100-((value/(AirValue-WaterValue))*100);
