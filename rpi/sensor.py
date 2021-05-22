43a13e80215d32f776a38a285b8cc52686883619
YashasKarthik1 <57079764+YashasKarthik1@users.noreply.github.com>
2021-05-23 01:16:10 +0530

    Update sensor.py

--- rpi/sensor.py
+++ rpi/sensor.py
@@ -1,8 +1,8 @@
 import serial
 
-arduino_port = "/dev/ttyACM0" #serial port of Arduino
-baud = 9600 #arduino uno runs at 9600 baud
-fileName="ldr.csv" #name of the CSV file generated
+arduino_port = "/dev/ttyACM0"
+baud = 9600 
+fileName="ldr.csv" 
 ser = serial.Serial(arduino_port, baud)
 print("Connected to Arduino port:" + arduino_port)
 file = open(fileName, "w")
@@ -11,17 +11,13 @@ print("Created file")
 line = 0
 
 while(True):
-
-    #display the data to the terminal
+    
     getData=str(ser.readline())
     data=getData[2:-5]
     print(data)
 
-    #add the data to the file
-    file = open(fileName, "a") #append the data to the file
-    file.write(data + "\n") #write data with a newline
-
+    file = open(fileName, "a") 
+    file.write(data + "\n") 
     file.close()
 
-#close out the file
 file.close()
