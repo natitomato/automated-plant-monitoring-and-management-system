4daae9e466d0d854084fd57133e8c190d3c60718
thundercookie <57059815+natitomato@users.noreply.github.com>
2021-05-23 01:16:37 +0530

    Update callibration.ino
    removed comments

--- Sensors/SoilMoistureSensor/callibration.ino
+++ Sensors/SoilMoistureSensor/callibration.ino
@@ -1,9 +1,9 @@
 void setup() {
-  Serial.begin(9600); // open serial port, set the baud rate as 9600 bps
+  Serial.begin(9600);
 }
 void loop() {
   int val;
-  val = analogRead(0); //connect sensor to Analog 0
-  Serial.println(val); //print the value to serial port
+  val = analogRead(0);
+  Serial.println(val);
   delay(100);
 }
